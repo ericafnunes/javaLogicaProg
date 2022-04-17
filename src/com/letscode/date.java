7a2c5b1e0f1593c30520ed4eaa7308b1e19a4d9c
Erica <eriica.f.n@hotmail.com>
2022-04-17 19:01:04 -0300

    exercicio de fixação de Lógica

---
+++ src/com/letscode/date.java
@@ -0,0 +1,17 @@
+package com.letscode;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
+
+public class date {
+    public static void main(String[] args){
+        LocalDate localDate = LocalDate.parse("2022-06-12");
+        System.out.println("Data: "+ localDate);
+
+        LocalDate novaData = localDate.minus(2, ChronoUnit.MONTHS);
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d-MM-uuuu");
+        System.out.println("Data: "+ novaData.format(dateFormatter));
+    }
+}
